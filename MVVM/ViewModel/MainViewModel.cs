4db8b9c29d8a90e0a2831238b8bd5db7aeb87d65
Studium <Studium@DESKTOP-KFMIAK4>
2024-03-10 23:08:42 +0100

    Add project files.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoteTakingApp.Core;
+
+namespace NoteTakingApp.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand NewNoteViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public NewNoteViewModel NewNoteVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        { 
+            HomeVM = new HomeViewModel();
+            NewNoteVM = new NewNoteViewModel();
+            CurrentView = HomeVM;
+
+
+            HomeViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = HomeVM;
+            });
+            NewNoteViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = NewNoteVM;
+            });
+
+            
+        }
+    }
+}
---
+++ MVVM/ViewModel/NewNoteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteTakingApp.MVVM.ViewModel
+{
+    internal class NewNoteViewModel
+    {
+    }
+}
